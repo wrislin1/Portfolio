5e8fc27066e47b38dae379404edaa62eaa728bc7
wrislin1 <60682090+wrislin1@users.noreply.github.com>
2020-03-15 23:46:59 -0400

    TicTacToe progress

--- scripts.js
+++ scripts.js
@@ -1,4 +1,36 @@
+var gameState=false;
+var moves=1;
 var currSlides=1;
+
+var game = {one:"",two:"",three:"",
+  four:"",five:"",six:"",
+  seven:"",eight:"",nine:"",won: function(p){
+    if((this.one==p&&this.two==p&&this.three==p)||
+  (this.four==p&&this.five==p&&this.six==p)||
+(this.seven==p&&this.eight==p&&this.nine==p)||
+(this.one==p&&this.four==p&&this.seven==p)||
+(this.two==p&&this.five==p&&this.eight==p)||
+(this.three==p&&this.six==p&&this.nine==p)||
+(this.one==p&&this.five==p&&this.nine==p)||
+(this.three==p&&this.five==p&&this.seven==p))
+{
+  return true;
+}
+else{return false;}
+},reset: function(){
+  this.one="";
+  this.two="";
+  this.three="";
+  this.four="";
+  this.five="";
+  this.six="";
+  this.seven="";
+  this.eight="";
+  this.nine="";
+}};
+
+
+
 show(currSlides);
 
 function show(n) {
@@ -21,4 +53,119 @@ function show(n) {
 function next(n) {
 
 show(currSlides += n);
+}
+
+function random() {
+  return Math.floor(Math.random() * (9 - 1 + 1) ) + 1;
+}
+
+function startGame(){
+  var i;
+  var cell = document.getElementsByClassName("cell");
+  for (i = 0; i < cell.length; i++) {
+      cell[i].innerHTML="";
+  }
+    game.reset();
+  gameState=true;
+}
+
+function endGame(){
+
+  gameState=false;
+
+
+}
+
+function comp(){
+
+
+  var rand=random();
+
+
+
+  var s;
+
+
+
+  var taken = true;
+    while(taken)
+    {
+
+      switch(rand)
+      {
+        case 1:
+        s = "one";
+        break;
+        case 2:
+        s = "two";
+        break;
+        case 3:
+        s = "three";
+        break;
+        case 4:
+        s = "four";
+        break;
+        case 5:
+        s = "five";
+        break;
+        case 6:
+        s = "six";
+        break;
+        case 7:
+        s = "seven";
+        break;
+        case 8:
+        s = "eight";
+        break;
+        case 9:
+        s = "nine";
+      }
+
+
+      if(game[s]=="x" || game[s]=="o")
+      {
+        rand=random();
+      }
+      else {
+        document.getElementById(s).innerHTML="O";
+        game[s]="o";
+        if(game.won("o"))
+        {
+          endGame();
+        }
+
+        taken=false;
+      }
+    }
+
+
+}
+
+
+
+
+function tictactoe(s){
+  var cell =  document.getElementsByClassName("cell");
+      var taken = true;
+  if(gameState==true){
+      if(game[s]!="x" && game[s]!="o")
+      {
+      document.getElementById(s).innerHTML="X";
+      game[s]="x";
+      taken=false;
+      }
+    if(taken==false)
+{
+      if(game.won("x")){
+        endGame();
+      }
+
+  if(gameState==true)
+    {
+      comp();
+    }
+  }
+
+  }
+
 }
