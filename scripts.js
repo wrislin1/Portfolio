769b26021af24f1a683d7b9e32510c221c24196a
wrislin1 <60682090+wrislin1@users.noreply.github.com>
2020-03-21 21:04:22 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 var turn=0;
 var gameState=false;
+var playerTurn=true;
 var moves=1;
 var currSlides=1;
 
@@ -28,7 +29,10 @@ else{return false;}
   this.seven="";
   this.eight="";
   this.nine="";
-}};
+  for(var k=0;k<this.xrows.length;k++){
+    this.xrows[k]=0;
+  }
+},xrows:[0,0,0,0,0,0,0,0]};
 
 
 
@@ -61,6 +65,7 @@ function random() {
 }
 
 function startGame(){
+  playerTurn=true;
   var i;
   var cell = document.getElementsByClassName("cell");
   for (i = 0; i < cell.length; i++) {
@@ -73,20 +78,241 @@ function startGame(){
 function endGame(){
   turn=0;
   gameState=false;
+playerTurn=false;
 
+}
 
+function setoRows(s){
+  switch (s) {
+    case "one":
+    game.xrows[0]=3;
+    game.xrows[3]=3;
+    game.xrows[6]=3;
+      break;
+    case "two":
+    game.xrows[1]=3;
+    game.xrows[3]=3;
+      break;
+      case "three":
+      game.xrows[2]=3;
+      game.xrows[3]=3;
+      game.xrows[7]=3;
+        break;
+        case "four":
+        game.xrows[0]=3;
+        game.xrows[4]=3;
+          break;
+          case "five":
+          game.xrows[1]=3;
+          game.xrows[4]=3;
+          game.xrows[6]=3;
+          game.xrows[7]=3;
+            break;
+          case "six":
+            game.xrows[2]=3;
+            game.xrows[4]=3;
+              break;
+          case "seven":
+              game.xrows[0]=3;
+              game.xrows[5]=3;
+              game.xrows[7]=3;
+                break;
+
+          case "eight":
+                game.xrows[1]=3;
+                game.xrows[5]=3;
+                  break;
+              case "nine":
+                  game.xrows[2]=3;
+                  game.xrows[5]=3;
+                  game.xrows[6]=3;
+                  break;
+}
+}
+
+function checkForWin(){
+for(var n=0;n<game.xrows.length;n++)
+{
+  if(game.xrows[n]==2)
+  {
+    return n;
+  }
+}
+return 10;
+}
+
+function blockwin(){
+  switch (checkForWin()) {
+    case 0:
+      if(game.one=="")
+      {
+        document.getElementById("one").innerHTML="O";
+        game.one="o";
+        game.xrows[0]=3;
+      }
+
+      else if (game.four=="") {
+        document.getElementById("four").innerHTML="O";
+        game.four="o";
+        game.xrows[0]=3;
+      }
+
+      else if (game.seven=="") {
+        document.getElementById("seven").innerHTML="O";
+        game.seven="o";
+        game.xrows[0]=3;
+      }
+      break;
+      case 1:
+      if(game.two=="")
+      {
+        document.getElementById("two").innerHTML="O";
+        game.two="o";
+        game.xrows[1]=3;
+      }
+
+      else if (game.five=="") {
+        document.getElementById("five").innerHTML="O";
+        game.five="o";
+        game.xrows[1]=3;
+      }
+
+      else if (game.eight=="") {
+        document.getElementById("eight").innerHTML="O";
+        game.eight="o";
+        game.xrows[1]=3;
+      }
+      break;
+      case 2:
+      if(game.three=="")
+      {
+        document.getElementById("three").innerHTML="O";
+        game.three="o";
+        game.xrows[2]=3;
+      }
+
+      else if (game.six=="") {
+        document.getElementById("six").innerHTML="O";
+        game.six="o";
+        game.xrows[2]=3;
+      }
+
+      else if (game.nine=="") {
+        document.getElementById("nine").innerHTML="O";
+        game.nine="o";
+        game.xrows[2]=3;
+      }
+      break;
+      case 3:
+      if(game.one=="")
+      {
+        document.getElementById("one").innerHTML="O";
+        game.one="o";
+        game.xrows[3]=3;
+      }
+
+      else if (game.two=="") {
+        document.getElementById("two").innerHTML="O";
+        game.two="o";
+        game.xrows[3]=3;
+      }
+
+      else if (game.three=="") {
+        document.getElementById("three").innerHTML="O";
+        game.three="o";
+        game.xrows[3]=3;
+      }
+      break;
+      case 4:
+      if(game.four=="")
+      {
+        document.getElementById("four").innerHTML="O";
+        game.four="o";
+        game.xrows[4]=3;
+      }
+
+      else if (game.five=="") {
+        document.getElementById("five").innerHTML="O";
+        game.five="o";
+        game.xrows[4]=3;
+      }
+
+      else if (game.six=="") {
+        document.getElementById("six").innerHTML="O";
+        game.six="o";
+        game.xrows[4]=3;
+      }
+      break;
+      case 5:
+      if(game.seven=="")
+      {
+        document.getElementById("seven").innerHTML="O";
+        game.seven="o";
+        game.xrows[5]=3;
+      }
+
+      else if (game.eight=="") {
+        document.getElementById("eight").innerHTML="O";
+        game.eight="o";
+        game.xrows[5]=3;
+      }
+
+      else if (game.nine=="") {
+        document.getElementById("nine").innerHTML="O";
+        game.nine="o";
+        game.xrows[5]=3;
+      }
+      break;
+      case 6:
+      if(game.one=="")
+      {
+        document.getElementById("one").innerHTML="O";
+        game.one="o";
+        game.xrows[6]=3;
+      }
+
+      else if (game.five=="") {
+        document.getElementById("five").innerHTML="O";
+        game.five="o";
+        game.xrows[6]=3;
+      }
+
+      else if (game.nine=="") {
+        document.getElementById("nine").innerHTML="O";
+        game.nine="o";
+        game.xrows[6]=3;
+      }
+      break;
+      case 7:
+      if(game.three=="")
+      {
+        document.getElementById("three").innerHTML="O";
+        game.three="o";
+        game.xrows[7]=3;
+      }
+
+      else if (game.five=="") {
+        document.getElementById("five").innerHTML="O";
+        game.five="o";
+        game.xrows[7]=3;
+      }
+
+      else if (game.seven=="") {
+        document.getElementById("seven").innerHTML="O";
+        game.seven="o";
+        game.xrows[7]=3;
+      }
+      break;
+
+  }
 }
 
 function comp(){
 
-
+  if(checkForWin()==10)
+{
   var rand=random();
-
-
-
   var s;
-
-
 
   var taken = true;
     while(taken)
@@ -128,33 +354,91 @@ function comp(){
         rand=random();
       }
       else {
+        setoRows(s);
         document.getElementById(s).innerHTML="O";
         game[s]="o";
-        if(game.won("o"))
-        {
-          endGame();
-        }
+
 
         taken=false;
       }
     }
-
-
+    if(game.won("o"))
+    {
+      endGame();
+    }
+}
+else {
+  blockwin();
+  if(game.won("o"))
+  {
+    endGame();
+  }
+}
+playerTurn=true;
 }
 
 
+function addRow(s){
+  switch (s) {
+    case "one":
+    game.xrows[0]++;
+    game.xrows[3]++;
+    game.xrows[6]++;
+      break;
+    case "two":
+    game.xrows[1]++;
+    game.xrows[3]++;
+      break;
+      case "three":
+      game.xrows[2]++;
+      game.xrows[3]++;
+      game.xrows[7]++;
+        break;
+        case "four":
+        game.xrows[0]++;
+        game.xrows[4]++;
+          break;
+          case "five":
+          game.xrows[1]++;
+          game.xrows[4]++;
+          game.xrows[6]++;
+          game.xrows[7]++;
+            break;
+          case "six":
+            game.xrows[2]++;
+            game.xrows[4]++;
+              break;
+          case "seven":
+              game.xrows[0]++;
+              game.xrows[5]++;
+              game.xrows[7]++;
+                break;
 
+          case "eight":
+                game.xrows[1]++;
+                game.xrows[5]++;
+                  break;
+              case "nine":
+                  game.xrows[2]++;
+                  game.xrows[5]++;
+                  game.xrows[6]++;
+                  break;
+}
+
+}
 
 function tictactoe(s){
   var cell =  document.getElementsByClassName("cell");
       var taken = true;
-  if(gameState==true){
+  if(gameState==true && playerTurn==true){
       if(game[s]!="x" && game[s]!="o")
       {
       document.getElementById(s).innerHTML="X";
       turn++;
       game[s]="x";
+      addRow(s);
       taken=false;
+      playerTurn=false;
       }
     if(taken==false)
 {
